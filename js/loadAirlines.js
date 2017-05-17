852bef5a6fa77f6c5fe2bfcb2c01fe12f87c943a
j1nma <j1nma@users.noreply.github.com>
2017-05-16 21:24:33 -0300

    Búsqueda de comentarios con modal en results. getLogos devuelve mapa de logos(id, url).

--- js/loadAirlines.js
+++ js/loadAirlines.js
@@ -1,45 +1,49 @@
-var showAirlines = (function loadAirlines() {
+$(document).ready(function() {
 
-    var URL = "http://hci.it.itba.edu.ar/v1/api/misc.groovy?method=getairlines&sort_key=name&sort_order=asc";
+    // Modal fix for select2.
+    $.fn.modal.Constructor.prototype.enforceFocus = function() {};
 
-    var myObj, html = "";
+    (function loadAirlines() {
 
-    $.getJSON(URL, function(result) {
+        var URL = "http://hci.it.itba.edu.ar/v1/api/misc.groovy?method=getairlines&sort_key=name&sort_order=asc";
 
-        myObj = result;
+        var myObj, html = "";
 
-        var airlines = myObj.airlines;
+        $.getJSON(URL, function(result) {
 
-        for (x in airlines) {
-            var logo = airlines[x].logo;
-            html += "<option value='" + airlines[x].id + "' data-image='" + airlines[x].logo + "'>" + airlines[x].name + "</option>";
+            myObj = result;
+
+            var airlines = myObj.airlines;
+
+            for (x in airlines) {
+                var logo = airlines[x].logo;
+                html += "<option value='" + airlines[x].id + "' data-image='" + airlines[x].logo + "'>" + airlines[x].name + "</option>";
+            }
+
+            $('#airlines_select').html(html);
+
+        });
+
+        $(".js-example-basic-single").select2({
+            templateResult: formatAirline
+        });
+
+    }());
+
+    function formatAirline(opt) {
+        if (!opt.id) {
+            return opt.text;
         }
 
-        document.getElementById("airlines_select").innerHTML = html;
+        var optimage = $(opt.element).data('image');
+        if (!optimage) {
+            return opt.text;
+        } else {
+            var $opt = $(
+                '<span><img src="' + optimage + '" alt="logo" /> ' + $(opt.element).text() + '</span>'
+            );
+            return $opt;
+        }
+    };
 
-    });
-
-    $(".js-example-basic-single").select2({
-        templateResult: formatAirline
-    });
-}());
-
-function formatAirline(opt) {
-    if (!opt.id) {
-        return opt.text;
-    }
-
-    var optimage = $(opt.element).data('image');
-    if (!optimage) {
-        return opt.text;
-    } else {
-        var $opt = $(
-            '<span class="userName"><img src="' + optimage + '" class="userPic" /> ' + $(opt.element).text() + '</span>'
-        );
-        return $opt;
-    }
-};
-
-$(".js-example-basic-single").select2({
-    templateResult: formatAirline
 });
